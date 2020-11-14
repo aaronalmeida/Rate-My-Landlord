4e20047a471e26468ad1771d1c70f1a094051638
Aaron <aaron.almeida5@gmail.com>
2020-11-14 13:16:19 -0500

    get and post requests working

--- ServerSide/Routes/review.js
+++ ServerSide/Routes/review.js
@@ -2,17 +2,33 @@ const express = require("express");
 const router = express.Router();
 const Review = require("../models/reviews");
 
-
-router.get("/", (req, res) => {
-  res.send("Review page lols");
+//Gets back all the reviews
+router.get("/", async (req, res) => {
+  try {
+    const reviews = await Review.find();
+    res.json(reviews);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 router.get("/specific", (req, res) => {
   res.send("Review page specific lol");
 });
 
-router.post("/", (req, res) => {
-  console.log(req.body);
+router.post("/", async (req, res) => {
+  const review = new Review({
+    address: req.body.address,
+    title: req.body.title,
+    review: req.body.review,
+  });
+
+  try {
+    const savedReview = await review.save();
+    res.json(savedReview);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 module.exports = router;
